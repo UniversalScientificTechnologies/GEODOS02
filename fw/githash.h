2654cded9382b9bb4cc89e065a7aa961390c2eac
kakl <kakl@users.noreply.github.com>
2023-02-19 09:46:47 +0000

    Merge branch 'GEODOS02C' of github.com:UniversalScientificTechnologies/GEODOS02 into GEODOS02C, extended build

--- fw/githash.h
+++ fw/githash.h
@@ -1,2 +1,2 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "c29a800e0bf23c1cf8931b2b66c9784bf539fa78,user_build";
+String githash = "fad32b7f5a59b9493b5a97f9414f288824e6205b,user_build";
