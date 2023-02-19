d731753ef5abcd00baecc5919eef1bec227b2698
kaklik <kaklik@users.noreply.github.com>
2023-02-19 20:57:19 +0000

    Fix absolute path to githash.h file. Swithed to relative., extended build

--- fw/githash.h
+++ fw/githash.h
@@ -1,2 +1,2 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "3365a44eb4c91e14ad5d27d21ba40e4350298f4a,user_build";
+String githash = "7d9064938ed47cf5dc08b1e0cc68983d24259d66,user_build";
